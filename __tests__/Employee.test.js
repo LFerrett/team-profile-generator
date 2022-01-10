d2ef38c0597e496d630ab256b84c5a6c8b2bc0f7
“Lauren <lferrett@gmail.com>
2022-01-09 23:35:45 -0500

    Changed syntax in test.

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,26 +1,34 @@
 const Employee = require("../lib/Employee");
-const testEmployee = new Employee ("Lauren, 232323, lferrett@gmail.com");
+const testEmployee = new Employee("Lauren, 232323, lferrett@gmail.com");
 
-describe('test to see if the Employee class returns name, id, and email', () => {
-  expect(testEmployee.name).toBe("Lauren");
-  expect(testEmployee.id).toBe("232323");
-  expect(testEmployee.email).toBe("lferrett@gmail.com");
+describe("Employee", () => {
+  it("test to see if the Employee class returns name, id, and email", () => {
+    expect(testEmployee.name).toBe("Lauren");
+    expect(testEmployee.id).toBe("232323");
+    expect(testEmployee.email).toBe("lferrett@gmail.com");
+  });
 });
 
-const Employee = require("../lib/Employee");
-
-describe('test to see if getName() returns name', () => {
-  expect(testEmployee.getName()).tobe("Lauren");
+describe("getName", () => {
+  it("Test to see if getName() returns name", () => {
+    expect(testEmployee.getName).tobe("Lauren");
+  });
 });
 
-describe('test to see if getID() returns id', () => {
-  expect(testEmployee.getID()).tobe("232323");
+describe("getID", () => {
+  it("Test to see if getID() returns ID", () => {
+    expect(testEmployee.getID).tobe("232323");
+  });
 });
 
-describe('test to see if getEmail() returns email', () => {
-  expect(testEmployee.getEmail()).tobe("lferrett@gmail.com");
+describe("getEmail", () => {
+  it("Test to see if getEmail() returns name", () => {
+    expect(testEmployee.getEmail).tobe("lferrett@gmail.com");
+  });
 });
 
-describe('test to see if getRole() returns role', () => {
-  expect(testEmployee.getRole()).tobe("Employee");
+describe("getRole", () => {
+  it("Test to see if getRole() returns role", () => {
+    expect(testEmployee.getRole).tobe("Employee");
+  });
 });
