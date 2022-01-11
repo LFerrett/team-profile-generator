2be012beabe5741fc02274eea58dd71497d082c5
“Lauren <lferrett@gmail.com>
2022-01-10 23:10:28 -0500

    Added intro prompt and styled html page.

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function initHTML() {
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <title>Dunder Mifflin Employee Directory</title>
     </head>
     <body>
@@ -150,7 +150,7 @@ function addHTML(member) {
             </div>
         </div>`;
     }
-    console.log("adding team member");
+    console.log("Team member added!");
     fs.appendFile("./output/team.HTML", data, function (err) {
       if (err) {
         return reject(err);
